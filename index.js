1eec66c5e9d9b04e612e9ca3238ef733694e1e72
aawezk456 <aawezk456@gmail.com>
2020-08-09 13:21:23 +0530

    changed mongo url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Connect to MongoDB
 mongoose
   .connect(
-    'mongodb://mongo:27017/docker-node-mongo',
+    'mongodb://mongo:27017/docker-node',
     { useNewUrlParser: true }
   )
   .then(() => console.log('MongoDB Connected'))
